ef5d64a725d38ea1db2f8c07a7248275ff144e44
papuht <papuht@utu.fi>
2021-10-25 18:22:31 +0300

    spawn

---
+++ Assets/V1/Scripts/SpawnPoint.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public int timer = 4000;
+    public GameObject player;
+    public GameObject spawn;
+    public double previousCheck;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.previousCheck = Time.time;
+
+       
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float posX = spawn.transform.position.x;
+        float posY = spawn.transform.position.y;
+        Vector3 pos = new Vector2(posX, posY); 
+         
+
+        
+        if (Time.time - this.previousCheck > 30) {
+            Instantiate(this.player, pos, Quaternion.identity);
+            player.tag = spawn.tag;
+            this.previousCheck = Time.time;
+            
+        }
+
+        
+    }
+}
